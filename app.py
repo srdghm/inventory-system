3fa549788231ff0a846dfe641307472cf6f3400a
= <=>
2026-07-10 10:55:30 +0545

    usable sidebar system added

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Static, Button
-from textual.containers import Horizontal, Vertical
+from widgets.sidebar import Sidebar
 
 class InventoryApp(App):
     CSS_PATH = "style.tcss"
     TITLE = "Inventory Management Made Easy"
 
     def compose(self) -> ComposeResult:
-        yield Header()
-        with Horizontal():
-            with Vertical(id = "sidebar"):
-                yield Button("Dashboard")
-                yield Button("Materials")
-                yield Button("Labour")
-                yield Button("Reports")
-            with Vertical(id= "content"):
-                yield Static("Welcome to Inventory Management System")
-        yield Footer()
+        yield Sidebar()
--- widgets/sidebar.py
+++ widgets/sidebar.py
@@ -1,1 +1,19 @@
+from textual.containers import Vertical
+from textual.app import ComposeResult
+from textual.widgets import Button, Label
 
+
+class Sidebar(Vertical):
+    MENU_ITEMS = [
+        {"id": "dashboard", "label": "Dashboard"},
+        {"id": "materials", "label": "Materials"},
+        {"id": "purchases", "label": "Purchases"},
+        {"id": "estimates", "label": "Estimates"},
+        {"id": "reports", "label": "Reports"},
+        {"id": "settings", "label": "Settings"},
+    ]
+    def compose(self) -> ComposeResult:
+
+        yield Label("Inventory Management", id="sidebar-title")
+        for item in self.MENU_ITEMS:
+            yield Button(item["label"], id=item["id"], classes="nav-button")
