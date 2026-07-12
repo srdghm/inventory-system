87d9b11a4e89212a83af781fce49566461cc2544
= <=>
2026-07-12 10:46:54 +0545

    mat screen redesign

--- screens/material_screen.py
+++ screens/material_screen.py
@@ -1,3 +1,5 @@
+from textual.containers import Horizontal,Vertical
+
 from messages import AddMaterialRequested, MaterialFormCancelRequested
 from textual.widget import Widget
 from textual.widgets import Label, Button
@@ -9,41 +11,41 @@ from widgets.add_material_dialouge import MaterialForm
 class MaterialScreen(Widget):
 
     def compose(self) -> ComposeResult:
-         yield Label("Materials")
-         yield Button(label="Add Material",id="add_material")
-         yield MaterialForm()
-         yield MaterialTable()
+        with Vertical():
+            yield Label("Materials")
+            with Horizontal():
+                yield MaterialForm()
+                yield MaterialTable()
+                with Vertical():
+                    yield Button(label="Add Material", id="add_material")
+                    yield Button(label="Delete Material", id="delete_material")
 
     def on_mount(self):
-         repository = MaterialRepository()
-
-         materials = repository.get_all()
-         form = self.query_one(MaterialForm)
-         form.hide()
-
-         table = self.query_one(MaterialTable)
-
-         table.load_materials(materials)
-
+        repository = MaterialRepository()
+        materials = repository.get_all()
+        form = self.query_one(MaterialForm)
+        form.hide()
+        table = self.query_one(MaterialTable)
+        table.load_materials(materials)
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
-         if event.button.id == "add_material":
-             form = self.query_one(MaterialForm)
-             form.show()
-             table = self.query_one(MaterialTable)
-             table.add_class("hidden")
-             self.notify("table hidden")
-             table.automatic_refresh()
+        if event.button.id == "add_material":
+            form = self.query_one(MaterialForm)
+            form.show()
+            table = self.query_one(MaterialTable)
+            table.add_class("hidden")
 
-    def  on_add_material_requested(self,message : AddMaterialRequested) ->None:
-         #self.notify("msg received")
-         repository =  MaterialRepository()
-         repository.add(message.material)
-         form = self.query_one(MaterialForm)
-         form.clear()
-         self.notify("material added")
+        elif event.button.id == "delete_material":
+            self.notify("Material Deleted")
 
-    def on_material_form_cancel_requested(self,message : MaterialFormCancelRequested):
+    def on_add_material_requested(self, message: AddMaterialRequested) -> None:
+        repository = MaterialRepository()
+        repository.add(message.material)
+        form = self.query_one(MaterialForm)
+        form.clear()
+        self.notify("material added")
+
+    def on_material_form_cancel_requested(self, message: MaterialFormCancelRequested):
         form = self.query_one(MaterialForm)
         form.hide()
         table = self.query_one(MaterialTable)
